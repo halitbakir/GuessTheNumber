0d63287f30fc77a925274b0a03e99078fc20f963
Halit Bakır <halitbakirx@gmail.com>
2023-07-26 00:28:55 +0300

    GuessTheNumber
    GuessTheNumber

---
+++ GuessTheNumber/GuessTheNumber/artirAzaltVC.swift
@@ -0,0 +1,64 @@
+//
+//  artirAzaltVC.swift
+//  GuessTheNumber
+//
+//  Created by Halit Bakır on 21.06.2023.
+//
+
+import UIKit
+
+class artirAzaltVC: UIViewController {
+
+    @IBOutlet weak var emojiImage: UIImageView!
+    
+    @IBOutlet weak var hakLabel: UILabel!
+    
+    @IBOutlet weak var artirAzaltLabel: UILabel!
+    
+    @IBOutlet weak var tahminGir: UITextField!
+    
+    
+    var dogruSayi: Int = 0
+    var kalanHak: Int = 5
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+        dogruSayi = Int.random(in: 1...20)
+        guncelleHakLabel()
+    }
+    
+    @IBAction func tahminEtTiklandi(_ sender: Any) {
+        if kalanHak > 0 {
+                if let tahmin = Int(tahminGir.text ?? "") {
+                    if tahmin < dogruSayi {
+                        artirAzaltLabel.text = "Arttır"
+                    } else if tahmin > dogruSayi {
+                        artirAzaltLabel.text = "Azalt"
+                    } else {
+                        artirAzaltLabel.text = "KAZANDIN!"
+                        kalanHak = 0 // Doğru tahmin edildiği için kalan hakları sıfırla
+                        emojiImage.image = UIImage(named: "basarili.png")
+                    }
+                } else {
+                    artirAzaltLabel.text = "Geçersiz giriş. Lütfen bir sayı girin."
+                }
+                
+                kalanHak -= 1 // Her tahminde bir hak düşür
+                guncelleHakLabel()
+                
+                if kalanHak == 0 {
+                    artirAzaltLabel.text = "KAYBETTİN! Doğru sayı: \(dogruSayi)"
+                    emojiImage.image = UIImage(named: "basarisiz.png")
+                }
+            }
+            
+            tahminGir.text = "" // Tahmin sonrası giriş alanını temizleyelim
+        }
+
+        func guncelleHakLabel() {
+            hakLabel.text = "Hak: \(kalanHak)"
+        }
+    }
+    
+
---
+++ GuessTheNumber/GuessTheNumber/tahminVC.swift
@@ -0,0 +1,28 @@
+//
+//  tahminVC.swift
+//  GuessTheNumber
+//
+//  Created by Halit Bakır on 21.06.2023.
+//
+
+import UIKit
+
+class tahminVC: UIViewController {
+    
+    @IBOutlet weak var tahminyazLabel: UILabel!
+    @IBOutlet weak var soruGorsel: UIImageView!
+    
+    @IBOutlet weak var tahminGiris: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func tahminEtTiklandi(_ sender: Any) {
+        
+    }
+    
+
+}
